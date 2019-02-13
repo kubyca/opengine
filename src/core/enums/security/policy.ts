08fd5f3c0d19afcaca93fa08051730668d6728b0
Mike G <mikegvso@gmail.com>
2019-02-13 15:19:30 -0500

    removing existing

--- src/core/enums/security/policy.ts
+++
@@ -1,9 +0,0 @@
-export enum Security {
-    POLICY_NONE,
-    POLICY_MINIMUM,
-    POLICY_STANDARD,
-    POLICY_DEFAULT,
-    POLICY_ALL
-}
-
-export default Security;
--- src/server/interfaces/ControllerInterface.ts
+++
@@ -1,8 +0,0 @@
-import * as express from 'express';
-
-export interface ControllerInterface {
-    router: express.Router;
-    setupRoutes();
-}
-
-export default ControllerInterface;
